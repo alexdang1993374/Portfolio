1b88cff4e76185875224983378fe7afc5e338b52
unknown <alexdang1993374@gmail.com>
2021-01-29 18:43:26 +0700

    set up router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutMe from "./pages/AboutMe";
+import ContactMe from "./pages/ContactMe";
+import MyWork from "./pages/MyWork";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutMe />
+      <Switch>
+        <Route path="/" exact>
+          <AboutMe />
+        </Route>
+        <Route path="/work">
+          <MyWork />
+        </Route>
+        <Route path="/contact">
+          <ContactMe />
+        </Route>
+      </Switch>
     </div>
   );
 }
