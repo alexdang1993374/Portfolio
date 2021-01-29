15a729c2ccc8089e6ec94e3d73583bc757b0619b
unknown <alexdang1993374@gmail.com>
2021-01-30 03:21:55 +0700

    inital motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 import home1 from "../img/home1.png";
-//Style
-import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>Contact me to get in touch about any job opportunities.</p>
         <button>Contact Me</button>
       </Description>
